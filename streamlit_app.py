852919310eb4c5aeb142cccce05b1fd953311b3e
root <ghasemi.a.ir@gmail.com>
2022-04-11 14:01:02 +0000

    Use T1 for table name

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,17 +13,17 @@ dfs = pd.read_html(url)
 indices = [i for i,df in enumerate(dfs) if len(df)>10 ]
 i = st.selectbox(label='Please choose the table in the page', options=indices, index  = 0)
 
-df  = dfs [i]
+T1  = dfs [i]
 
 
-df.columns = [col if isinstance(col, str) else ' - '.join(dict.fromkeys(col)).strip() for col in df.columns.values] # https://stackoverflow.com/questions/14507794/pandas-how-to-flatten-a-hierarchical-index-in-columns
-df.columns = [re.sub("[\[].*?[\]]", "", c) for c in df.columns.values]
+T1.columns = [col if isinstance(col, str) else ' - '.join(dict.fromkeys(col)).strip() for col in T1.columns.values] # https://stackoverflow.com/questions/14507794/pandas-how-to-flatten-a-hierarchical-index-in-columns
+T1.columns = [re.sub("[\[].*?[\]]", "", c) for c in T1.columns.values]
 
-print(df.columns)
+print(T1.columns)
 
 aliases = dict({})
 md_table = []
-for i,(col, dt) in enumerate(df.dtypes.items()):
+for i,(col, dt) in enumerate(T1.dtypes.items()):
     alias = f'C{i+1}'
     dt = str(dt)
     dt = 'Text' if dt=='object' else 'Number' if 'float' in dt else dt
@@ -34,7 +34,7 @@ st.markdown('Columns')
 md_table = pd.DataFrame(data=md_table, columns=('Column Alias', 'Column Name', 'Column Type'), index = None)
 st.table(md_table)
 
-query = st.text_input(label= 'Please enter SQL query', value = 'SELECT  * from df')
+query = st.text_input(label= 'Please enter SQL query', value = 'SELECT  * from T1')
 
 for alias,column  in aliases.items(): query = query.replace(alias, column)
 
