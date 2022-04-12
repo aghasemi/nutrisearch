ca7a3a4315da4226c5dc499d4d3ed8640f6a0821
root <ghasemi.a.ir@gmail.com>
2022-04-12 20:00:06 +0000

    Add date type

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ st.set_page_config(layout='wide')
 url = st.text_input(label='Please enter page address', value = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(PPP)_per_capita')
 
 with st.expander(label = 'Modify thousands and decimal separator character'):
-    num_sep = st.selectbox(label = 'Thousands separator', options=[',', '.', '\xa0'], index=0)
-    dec_sep = st.selectbox(label = 'Thousands separator', options=[',', '.'], index=1)
+    num_sep = st.selectbox(label = 'Thousands separator', options=[',', '.', '\xa0'], index=1 if '://de.' in url else 0)
+    dec_sep = st.selectbox(label = 'Thousands separator', options=[',', '.'], index=0 if '://de.' in url else 0)
 
 
 dfs = pd.read_html(url, thousands=num_sep, decimal=dec_sep)
@@ -43,26 +43,27 @@ else:
     T1.columns = [col if isinstance(col, str) else ' - '.join(dict.fromkeys(col)).strip() for col in T1.columns.values] # https://stackoverflow.com/questions/14507794/pandas-how-to-flatten-a-hierarchical-index-in-columns
     T1.columns = [re.sub("[\[].*?[\]]", "", c) for c in T1.columns.values]
 
-    data_types_list = ['Text', 'Integer', 'Float']
+    data_types_list = ['Text', 'Integer', 'Float', 'Date']
     data_types = dict({x:i for i,x in enumerate(data_types_list)})
 
     with st.expander(label = 'Modify data types of columns'):
         aliases = dict({})
         md_table = []
         for i,(col, dt) in enumerate(T1.dtypes.items()):
-            alias = f'C{i+1}'
+            alias = f'C{i+1}$'
             dt = str(dt)
             dt = 'Integer' if 'int' in dt else 'Float' if 'float' in dt  else 'Text'
             aliases[alias] = f'"{col}"'
 
             #c1, c2 =  st.columns([1,3])
             #c1.markdown(f'{i+1:3}. {alias} {col}')
-            dt = st.selectbox(label = f'Please choose data type for {col}', options = data_types_list, key = col, index = data_types[dt])
+            dt = st.selectbox(label = f'Please choose data type for {col}', options = data_types_list, key = col, index = data_types.get(dt, 0))
             
             if dt=='Text': T1[col] = T1[col].apply(lambda s: re.sub("[\[].*?[\]]", "", str(s)).strip('*').strip())
             if dt=='Integer': T1[col] = T1[col].apply(lambda s: extract_number(str(s), num_sep=num_sep, dec_sep = dec_sep, is_float = False))
             if dt=='Float': T1[col] = T1[col].apply(lambda s: extract_number(str(s), num_sep=num_sep, dec_sep = dec_sep, is_float = True))
-            
+            if dt=='Date': T1[col] = pd.to_datetime(T1[col].apply(lambda s: re.sub("[\[].*?[\]]", "", str(s)).strip('*').strip()), errors='coerce')
+
             md_table += [[alias, str(col), str(dt)]]
 
 
@@ -75,6 +76,10 @@ else:
     for alias,column  in aliases.items(): query = query.replace(alias, column)
 
     print(query)
+    try:
+        result = duckdb.query(query).to_df()
+        st.dataframe(result, width = 20 * len(''.join(result.columns.values)), height= 10 * len(result))
 
+    except Exception as e:
+        st.markdown(f'SQL Error: {e}')
 
-    st.write(duckdb.query(query).to_df())
